2b4af9473da5a461ef3aa4060328962e16ae4c37
soyenle <34003821+soyenle@users.noreply.github.com>
2021-11-26 11:05:35 +0100

    Update events.js

--- events.js
+++ events.js
@@ -92,9 +92,6 @@ AFRAME.registerComponent('listener1', {
     AFRAME.registerComponent('listener0', {
     init: function () {
     document.getElementById('Distance1').textContent = `Distanz:${document.getElementById('Model1').getAttribute('distanceMsg')}`;
-      
-    doc.textContent = "Instructions";
-    document.getElementById("btn").onclick = Quiz0;
     
       Distance_Show();
     

c220162b070213b11538e0ac4c4962b68b70a828
soyenle <34003821+soyenle@users.noreply.github.com>
2021-11-30 20:35:24 +0100

    Update events.js

--- events.js
+++ events.js
@@ -1,62 +1,5 @@
 
 
-// Component to make a popup window for the quiz.
-AFRAME.registerComponent('listener1', {
-  init: function () {
-    this.el.addEventListener('mouseenter', function (evt) {
-        var doc = document.getElementById("btn");             
-            // Changing the text content
-            doc.textContent = "Quiz1";
-            document.getElementById("btn").onclick = Quiz1;    
-    });
-  }
-});
-
-     AFRAME.registerComponent('listener2', {
-  init: function () {
-    this.el.addEventListener('mouseenter', function (evt) {
-        var doc = document.getElementById("btn");             
-            // Changing the text content
-            doc.textContent = "Quiz2";
-            document.getElementById("btn").onclick = Quiz2;      
-    }); }});
-
-      AFRAME.registerComponent('listener3', {
-  init: function () {
-    this.el.addEventListener('mouseenter', function (evt) {
-        var doc = document.getElementById("btn");             
-            // Changing the text content
-            doc.textContent = "Quiz3";
-            document.getElementById("btn").onclick = Quiz3;      
-    }); }});
-
-      AFRAME.registerComponent('listener4', {
-  init: function () {
-    this.el.addEventListener('mouseenter', function (evt) {
-        var doc = document.getElementById("btn");             
-            // Changing the text content
-            doc.textContent = "Quiz4";
-            document.getElementById("btn").onclick = Quiz4;      
-    }); }});
-
-   AFRAME.registerComponent('listener5', {
-  init: function () {
-    this.el.addEventListener('mouseenter', function (evt) {
-        var doc = document.getElementById("btn");             
-            // Changing the text content
-            doc.textContent = "Quiz5";
-            document.getElementById("btn").onclick = Quiz5;      
-    }); }});
-
-   AFRAME.registerComponent('listener6', {
-  init: function () {
-    this.el.addEventListener('mouseenter', function (evt) {
-        var doc = document.getElementById("btn");             
-            // Changing the text content
-            doc.textContent = "Quiz6";
-            document.getElementById("btn").onclick = Quiz6;      
-    }); }});
-
 
          function Quiz0() {           
             window.alert("Begebe dich in die nähe eines 3D-Modells (<15m) \nDanach lässt sich ein Quizfenster öffnen");
@@ -94,10 +37,6 @@ AFRAME.registerComponent('listener1', {
     AFRAME.registerComponent('listener0', {
     init: function () {
     document.getElementById('Distance1').textContent = `Distanz:${document.getElementById('Model1').getAttribute('distanceMsg')}`;
-      Distance_Show();
-    
-    
-    
-    
+      Distance_Show();    
   }
 });
